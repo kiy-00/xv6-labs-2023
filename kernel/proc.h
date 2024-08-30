94d2c6dd5c18964359f9e47c0d463555c8944206
kiy-00 <2927703699@qq.com>
2024-08-30 11:01:36 +0800

    speed up system calls is completed

--- kernel/proc.h
+++ kernel/proc.h
@@ -84,6 +84,8 @@ enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 // Per-process state
 struct proc {
   struct spinlock lock;
+  
+  struct usyscall *usyscallpage;  // share page whithin kernel and user
 
   // p->lock must be held when using these:
   enum procstate state;        // Process state
