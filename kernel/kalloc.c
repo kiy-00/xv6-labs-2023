85a39b41f1ab2ece428b244d28c6856ebc53639d
kiy-00 <2927703699@qq.com>
2024-08-31 22:14:01 +0800

    fix bug

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,9 +23,10 @@ struct {
   struct run *freelist;
 } kmem;
 
-// the reference count of physical memory page
-int useReference[PHYSTOP/PGSIZE];
-struct spinlock ref_count_lock;
+// 新增了页面引用计数数组
+volatile int page_refcnt[PHYSTOP/PGSIZE]; // COW reference count. 
+
+
 
 void
 kinit()
@@ -50,21 +51,19 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  if(page_refcnt[(uint64)pa/PGSIZE]>0)
+  {
+    page_refcnt[(uint64)pa/PGSIZE] -= 1;
+    // 如果页面仍有引用，直接返回，不释放页面
+  }
+  if(page_refcnt[(uint64)pa/PGSIZE]>0) return;
+  
   struct run *r;
-  int temp;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  acquire(&ref_count_lock);
-  // decrease the reference count, if use reference is not zero, then return
-  useReference[(uint64)pa/PGSIZE] -= 1;
-  temp = useReference[(uint64)pa/PGSIZE];
-  release(&ref_count_lock);
-  if (temp > 0)
-    return;
-
-  // Fill with junk to catch dangling refs.
+  // 用垃圾数据填充内存，以捕捉悬挂引用
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
@@ -85,16 +84,12 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r) {
+  if(r)
     kmem.freelist = r->next;
-    acquire(&ref_count_lock);
-    // initialization the ref count to 1
-    useReference[(uint64)r / PGSIZE] = 1;
-    release(&ref_count_lock);
-  }
   release(&kmem.lock);
 
   if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+    memset((char*)r, 5, PGSIZE); // 用垃圾数据填充内存
+  page_refcnt[(uint64)r/PGSIZE] = 1; // 设置引用计数为1
   return (void*)r;
 }
